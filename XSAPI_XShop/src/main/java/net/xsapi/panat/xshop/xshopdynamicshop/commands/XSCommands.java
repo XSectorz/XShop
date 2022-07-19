4b64a084680fe227e2f82b715e2e3afb87d9f95f
XSectorz <81065047+XSectorz@users.noreply.github.com>
2022-07-19 11:29:23 +0700

    [U] setStock/setPrice/reload/Fix Bug(s)
    [+] เพิ่มคำสั่ง setStock สำหรับเปลี่ยนจำนวนสินค้า
    [+] เพิ่มคำสั่ง setPrice สำหรับตั้งราคาสินค้า อิงราคาตาม buy | sell = buy_price * 75 /100
    [+] เพิ่มคำสั่ง reload 
    [+] เเก้บัคการรรแสดงผลทางภาษาเล็กน้อย

--- XSAPI_XShop/src/main/java/net/xsapi/panat/xshop/xshopdynamicshop/commands/XSCommands.java
+++ XSAPI_XShop/src/main/java/net/xsapi/panat/xshop/xshopdynamicshop/commands/XSCommands.java
@@ -1,9 +1,9 @@
 package net.xsapi.panat.xshop.xshopdynamicshop.commands;
 
+import net.xsapi.panat.xshop.xshopdynamicshop.configuration.config;
 import net.xsapi.panat.xshop.xshopdynamicshop.configuration.messages;
 import net.xsapi.panat.xshop.xshopdynamicshop.configuration.storages;
-import net.xsapi.panat.xshop.xshopdynamicshop.core.XShopDynamicShopCore;
-import net.xsapi.panat.xshop.xshopdynamicshop.core.XShopType;
+import net.xsapi.panat.xshop.xshopdynamicshop.core.*;
 import net.xsapi.panat.xshop.xshopdynamicshop.gui.XShop;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -24,7 +24,6 @@ public class XSCommands implements CommandExecutor {
             if(command.getName().equalsIgnoreCase("xshop")) {
                 if (args.length == 0) {
                     if(sender.hasPermission("xshop.use")) {
-                        sender.sendMessage("Opened Shop!");
                         XShopDynamicShopCore.shopPage.put(sender.getUniqueId(),1);
                         XShopDynamicShopCore.shopType.put(sender.getUniqueId(),XShopType.NoneType);
                         XShop.openInv(sender, XShopDynamicShopCore.shopType.get(sender.getUniqueId()),
@@ -35,21 +34,44 @@ public class XSCommands implements CommandExecutor {
                         return false;
                     }
                 } else if(args.length == 1) {
-                    sender.sendMessage((messages.customConfig.getString("cmd_xshop")).replace("&","§"));
-                    sender.sendMessage((messages.customConfig.getString("cmd_xshop_help")).replace("&","§"));
-                    if(sender.hasPermission("xshop.storages.save")) {
-                        sender.sendMessage((messages.customConfig.getString("cmd_xshop_save")).replace("&","§"));
+                    if((args[0].equalsIgnoreCase("help"))) {
+                        sender.sendMessage((messages.customConfig.getString("cmd_xshop")).replace("&","§"));
+                        sender.sendMessage((messages.customConfig.getString("cmd_xshop_help")).replace("&","§"));
+                        if(sender.hasPermission("xshop.storages.save")) {
+                            sender.sendMessage((messages.customConfig.getString("cmd_xshop_save")).replace("&","§"));
+                        }
+                        if(sender.hasPermission("xshop.storages.load")) {
+                            sender.sendMessage((messages.customConfig.getString("cmd_xshop_load")).replace("&","§"));
+                        }
+                        if(sender.hasPermission("xshop.storages.remove")) {
+                            sender.sendMessage((messages.customConfig.getString("cmd_xshop_remove")).replace("&","§"));
+                        }
+                        if(sender.hasPermission("xshop.storages.give")) {
+                            sender.sendMessage((messages.customConfig.getString("cmd_xshop_give")).replace("&","§"));
+                        }
+                        if(sender.hasPermission("xshop.storages.reload")) {
+                            sender.sendMessage((messages.customConfig.getString("cmd_xshop_reload")).replace("&","§"));
+                        }
+                        if(sender.hasPermission("xshop.setstock")) {
+                            sender.sendMessage((messages.customConfig.getString("cmd_xshop_setstock")).replace("&","§"));
+                        }
+                        if(sender.hasPermission("xshop.setprice")) {
+                            sender.sendMessage((messages.customConfig.getString("cmd_xshop_setprice")).replace("&","§"));
+                        }
+                        return true;
+                    } else if(args[0].equalsIgnoreCase("reload")){
+                        if(!sender.hasPermission("xshop.reload")) {
+                            sender.sendMessage((XShopDynamicShopCore.prefix + messages.customConfig.getString("no_perms")).replace("&","§"));
+                            return false;
+                        }
+                        XShopDynamicShopCore.saveData();
+                        XShopDynamicShopCore.loadData();
+
+                        messages.reload();
+                        config.reload();
+                        sender.sendMessage((XShopDynamicShopCore.prefix + messages.customConfig.getString("reload_complete")).replace("&","§"));
+                        return true;
                     }
-                    if(sender.hasPermission("xshop.storages.load")) {
-                        sender.sendMessage((messages.customConfig.getString("cmd_xshop_load")).replace("&","§"));
-                    }
-                    if(sender.hasPermission("xshop.storages.remove")) {
-                        sender.sendMessage((messages.customConfig.getString("cmd_xshop_remove")).replace("&","§"));
-                    }
-                    if(sender.hasPermission("xshop.storages.give")) {
-                        sender.sendMessage((messages.customConfig.getString("cmd_xshop_give")).replace("&","§"));
-                    }
-                    return true;
                 } else if(args.length == 2) {
                     if((args[0].equalsIgnoreCase("save"))) {
                         if(!sender.hasPermission("xshop.storages.save")) {
@@ -117,6 +139,10 @@ public class XSCommands implements CommandExecutor {
                     }
                 } else if(args.length == 3) {
                     if(args[0].equalsIgnoreCase("give")) {
+                        if(!sender.hasPermission("xshop.storages.give")) {
+                            sender.sendMessage((XShopDynamicShopCore.prefix + messages.customConfig.getString("no_perms")).replace("&","§"));
+                            return false;
+                        }
                         String name = args[1].toString();
 
                         if(storages.customConfig.get(name) == null) {
@@ -142,6 +168,122 @@ public class XSCommands implements CommandExecutor {
                                 .replace("%item%",name).replace("%name%",playerName).replace("&", "§"));
                         return true;
                     }
+                } else if(args.length == 4) {
+                     if(args[0].equalsIgnoreCase("setstock")) {
+                        if(!sender.hasPermission("xshop.setstock")) {
+                            sender.sendMessage((XShopDynamicShopCore.prefix + messages.customConfig.getString("no_perms")).replace("&","§"));
+                            return false;
+                        }
+                        String type = args[1].toString();
+                        String name = args[2].toString();
+                        int amount = 0;
+
+                        try {
+                            amount = Integer.parseInt(args[3].toString());
+                        } catch (NumberFormatException nfe) {
+                            sender.sendMessage((XShopDynamicShopCore.prefix + messages.customConfig.getString("not_a_number")).replace("&", "§"));
+                            return false;
+                        }
+
+                        if(amount <= 0) {
+                            if(amount != -1) {
+                                sender.sendMessage((XShopDynamicShopCore.prefix + messages.customConfig.getString("stock_only_positive")).replace("&", "§"));
+                                return false;
+                            }
+                        }
+
+                        XShopType typeShop = null;
+                        try {
+                            type = type.toLowerCase();
+                            type = type.substring(0, 1).toUpperCase() + type.substring(1);
+                            typeShop = XShopType.valueOf(type);
+                        } catch (IllegalArgumentException e) {
+                            sender.sendMessage((XShopDynamicShopCore.prefix + messages.customConfig.getString("type_of_shop_null")).replace("&", "§"));
+                            return false;
+                        }
+
+                        XShopItems shopItems = XShopDynamicShopCore.getItemsByPrivateNameAndShop(typeShop,name);
+
+                        if(shopItems == null) {
+                            sender.sendMessage((XShopDynamicShopCore.prefix + messages.customConfig.getString("item_of_shop_null")).replace("&", "§"));
+                            return false;
+                        }
+
+                        shopItems.setStock(amount);
+
+                        if(amount == -1) {
+                            amount = 1;
+                        }
+
+                        shopItems.setPreviousPrice(shopItems.getValue()*shopItems.getMedian()/amount);
+
+                        sender.sendMessage((XShopDynamicShopCore.prefix + messages.customConfig.getString("setstock_complete")).replace("&", "§"));
+                        XShopDynamicShopCore.saveData();
+                        return true;
+                    } else if(args[0].equalsIgnoreCase("setprice")) {
+                         if(!sender.hasPermission("xshop.setprice")) {
+                             sender.sendMessage((XShopDynamicShopCore.prefix + messages.customConfig.getString("no_perms")).replace("&","§"));
+                             return false;
+                         }
+
+                         String type = args[1].toString();
+                         String name = args[2].toString();
+                         double amount = 0;
+
+                         try {
+                             amount = Double.parseDouble(args[3].toString());
+                         } catch (NumberFormatException nfe) {
+                             sender.sendMessage((XShopDynamicShopCore.prefix + messages.customConfig.getString("not_a_number")).replace("&", "§"));
+                             return false;
+                         }
+
+                         if(amount <= 0) {
+                             sender.sendMessage((XShopDynamicShopCore.prefix + messages.customConfig.getString("price_only_positive")).replace("&", "§"));
+                             return false;
+                         }
+
+                         XShopType typeShop = null;
+                         try {
+                             type = type.toLowerCase();
+                             type = type.substring(0, 1).toUpperCase() + type.substring(1);
+                             typeShop = XShopType.valueOf(type);
+                         } catch (IllegalArgumentException e) {
+                             sender.sendMessage((XShopDynamicShopCore.prefix + messages.customConfig.getString("type_of_shop_null")).replace("&", "§"));
+                             return false;
+                         }
+
+                         XShopItems shopItems = XShopDynamicShopCore.getItemsByPrivateNameAndShop(typeShop,name);
+
+                         if(shopItems == null) {
+                             sender.sendMessage((XShopDynamicShopCore.prefix + messages.customConfig.getString("item_of_shop_null")).replace("&", "§"));
+                             return false;
+                         }
+
+                         int stock = (int) shopItems.getStock();
+                         if(stock == -1) {
+                             stock = 1;
+                         }
+
+                         double val = amount*stock;
+
+                         if(shopItems.getItemsType().equals(XShopItemsType.CUSTOM)) {
+                             XShopItemsCustom shopItemsCustom = (XShopItemsCustom) shopItems;
+                             if(shopItemsCustom.getCustomType().equalsIgnoreCase("sell")) {
+                                 val = (val*100)/75;
+                             }
+                         } else {
+                             if(shopItems.getItemsType().equals(XShopItemsType.SELL_ONLY)) {
+                                 val = (val * 100)/75;
+                             }
+                         }
+
+                         shopItems.setMedian(1);
+                         shopItems.setValue(val);
+                         shopItems.setPreviousPrice(shopItems.getValue()*shopItems.getMedian()/stock);
+                         sender.sendMessage((XShopDynamicShopCore.prefix + messages.customConfig.getString("setprice_complete")).replace("&", "§"));
+                         XShopDynamicShopCore.saveData();
+                         return true;
+                     }
                 }
             }
             sender.sendMessage((XShopDynamicShopCore.prefix + messages.customConfig.getString("cmd_not_found"))
@@ -150,6 +292,17 @@ public class XSCommands implements CommandExecutor {
         } else { //Console
 
             if(command.getName().equalsIgnoreCase("xshop")) {
+                if(args.length == 1) {
+                    if(args[0].equalsIgnoreCase("reload")) {
+                        XShopDynamicShopCore.saveData();
+                        XShopDynamicShopCore.loadData();
+
+                        messages.reload();
+                        config.reload();
+                        commandSender.sendMessage("§c[Xshop] §aReload complete!");
+                        return true;
+                    }
+                }
                 if(args.length == 3) {
                     if(args[0].equalsIgnoreCase("give")) {
                         String name = args[1].toString();
